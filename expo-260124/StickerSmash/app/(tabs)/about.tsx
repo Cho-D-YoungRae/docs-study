f102d1ba13e4258d47557a8f2436eb7f67faf3ad
Cho-D-YoungRae <yrc9229@gmail.com>
2026-01-24 14:13:44 +0900

    expo: Add navigation

---
+++ expo-260124/StickerSmash/app/(tabs)/about.tsx
@@ -0,0 +1,21 @@
+import {View, StyleSheet, Text} from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Abouts screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
